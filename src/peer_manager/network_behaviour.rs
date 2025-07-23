da7be38bbbc26468268d883b6f62e8720b9181ab
Tumas <tumas@grandine.io>
2025-07-23 14:22:24 +0300

    Backport 83cad25d9 Fix Rust 1.88 clippy errors & execution engine tests

--- src/peer_manager/network_behaviour.rs
+++ src/peer_manager/network_behaviour.rs
@@ -99,14 +99,14 @@ impl NetworkBehaviour for PeerManager {
         if let Some(enr) = self.peers_to_dial.pop() {
             self.inject_peer_connection(&enr.peer_id(), ConnectingType::Dialing, Some(enr.clone()));
 
+            let multiaddr_quic = if self.quic_enabled {
+                enr.multiaddr_quic()
+            } else {
+                vec![]
+            };
+
             // Prioritize Quic connections over Tcp ones.
-            let multiaddrs = [
-                self.quic_enabled
-                    .then_some(enr.multiaddr_quic())
-                    .unwrap_or_default(),
-                enr.multiaddr_tcp(),
-            ]
-            .concat();
+            let multiaddrs = [multiaddr_quic, enr.multiaddr_tcp()].concat();
 
             debug!(self.log, "Dialing peer"; "peer_id"=> %enr.peer_id(), "multiaddrs" => ?multiaddrs);
             return Poll::Ready(ToSwarm::Dial {
